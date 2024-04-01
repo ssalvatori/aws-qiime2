c5836f63be6bf97239deb948a7f7bfedf6f2c1ea
Stefano Salvatori <ssalvatori@gmail.com>
2024-04-01 21:07:43 +0200

    remove s3 bucket

--- terraform/s3_bucket.tf
+++ terraform/s3_bucket.tf
@@ -2,34 +2,34 @@
 # Create S3 bucket
 #
 
-module "s3" {
-  source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "4.1.1"
-
-  bucket = local.bucket_name
-
-
-  acl                     = "private"
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-
-  control_object_ownership = true
-  object_ownership         = "BucketOwnerPreferred"
-
-  force_destroy = true
-
-  expected_bucket_owner = data.aws_caller_identity.current.account_id
-
-  server_side_encryption_configuration = {
-    rule = { apply_server_side_encryption_by_default = {
-      sse_algorithm = "AES256"
-      }
-    }
-  }
-
-}
+# module "s3" {
+#   source  = "terraform-aws-modules/s3-bucket/aws"
+#   version = "4.1.1"
+#
+#   bucket = local.bucket_name
+#
+#
+#   acl                     = "private"
+#   block_public_acls       = true
+#   block_public_policy     = true
+#   ignore_public_acls      = true
+#   restrict_public_buckets = true
+#
+#   control_object_ownership = true
+#   object_ownership         = "BucketOwnerPreferred"
+#
+#   force_destroy = true
+#
+#   expected_bucket_owner = data.aws_caller_identity.current.account_id
+#
+#   server_side_encryption_configuration = {
+#     rule = { apply_server_side_encryption_by_default = {
+#       sse_algorithm = "AES256"
+#       }
+#     }
+#   }
+#
+# }
 
 resource "aws_iam_role" "this" {
   name = "qiime2-ec2-role"
